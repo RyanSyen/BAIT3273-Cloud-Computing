ad79b5fa3bb52a6a8672046fe3e411d8658413d6
RyanSyen <ryanwong710200@gmail.com>
2022-09-19 16:15:05 +0800

    function done

--- admin/EmpAppUpdated.py
+++ admin/EmpAppUpdated.py
@@ -246,8 +246,62 @@ def account():
 
 @adminBlueprint.route("/updateEmp")
 def updateEmp():
-    return getEmp1()
-    # return render_template('updateEmp.html')
+    return render_template('updateEmp.html')
+
+
+@adminBlueprint.route("/updateEmpSubmit", methods=['POST'])
+def updateSubmitEmp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    emp_image_file = request.files['emp_image_file']
+
+    update_sql = "UPDATE employee SET first_name = %s, last_name = %s, pri_skill = %s, location = %s WHERE emp_id = %s"
+    sql_update = "UPDATE employee SET first_name=%s, last_name=%s, pri_skill=%s, location=%s WHERE emp_id = %s % (first_name, last_name, pri_skill, location, emp_id)"
+
+    cursor = db_conn.cursor()
+
+    if emp_image_file.filename == "":
+        return "Please select a file"
+
+    try:
+
+        cursor.execute(update_sql, (first_name, last_name,
+                       pri_skill, location, emp_id))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(
+                Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client(
+                's3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('home.html')
 
 
 @adminBlueprint.route("/deleteEmp")
